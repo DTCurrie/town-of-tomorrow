34f26c147b5c883471b015f60c6f13f5b4e7dd25
Devin T. Currie <devintcurrie@gmail.com>
2023-01-20 16:37:40 -0500

    Add config for GitHub pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@ import adapter from '@sveltejs/adapter-static';
 // import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -9,6 +11,15 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		// Setup path for Github Pages
+		paths: {
+			base: dev ? '' : '/town-of-tomorrow',
+		},
+
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'internal', etc.
+		appDir: 'internal',
+
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
